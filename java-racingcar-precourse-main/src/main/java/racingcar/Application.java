868936c60676cb2bf59a0cf500cf0390a0eedfd8
skullKim <kyk990328@gmail.com>
2021-12-16 21:23:22 +0900

    feat(RacingGameInputController.java): 자동차 이름 검증, 파싱 로직 완료
    자동차 이름들을 입력받고 검증한 뒤 검증이 통과되면 알맞은 데이터
    형식으로 반환한다. 검증에 실패하면 다시 입력받는다

--- java-racingcar-precourse-main/src/main/java/racingcar/Application.java
+++ java-racingcar-precourse-main/src/main/java/racingcar/Application.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import racingcar.controller.RacingGameInputController;
+import racingcar.utils.DataValidation;
+import racingcar.view.RacingGameInput;
+
 public class Application {
     public static void main(String[] args) {
         // TODO 구현 진행
+		RacingGameInputController a = new RacingGameInputController(new DataValidation(),
+			new RacingGameInput());
+		a.inputCarsName();
 	}
 }
---
+++ java-racingcar-precourse-main/src/main/java/racingcar/controller/RacingGameInputController.java
@@ -0,0 +1,47 @@
+package racingcar.controller;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import racingcar.model.Car;
+import racingcar.utils.DataValidation;
+import racingcar.view.RacingGameInput;
+
+public class RacingGameInputController {
+
+	private DataValidation validation;
+
+	private RacingGameInput input;
+
+	public RacingGameInputController(final DataValidation validation,
+			final RacingGameInput input) {
+		this.validation = validation;
+		this.input = input;
+	}
+
+	public List<String> parseCarsName(final String carsName) {
+		String[] carsNameArray = carsName.split(",");
+		return Arrays.asList(carsNameArray);
+	}
+
+	public ArrayList<Car> inputCarsName() {
+		final ArrayList<Car> cars = new ArrayList<>();
+		while(true) {
+			try {
+				final String carsName = input.inputCarsName();
+				if(validation.validateCarName(carsName)) {
+					final List<String> carNameList = parseCarsName(carsName);
+					carNameList.forEach(carName -> cars.add(new Car(carName)));
+					break;
+				}
+			} catch (IllegalArgumentException error) {
+				System.out.println(error.getMessage());
+			}
+		}
+		return cars;
+	}
+
+
+}
